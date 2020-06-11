d5f143c5eed7f6776705c0eb0db9a844926d9c9d
attisvalentin <attisvalentin@users.noreply.github.com>
2020-06-11 19:01:42 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,3 @@ function hamburgerbutton(x) {
     x.classList.toggle("change");
     document.getElementById("dropdown").classList.toggle("show");
 }
-
-function skills() {
-    document.getElementById("about-me").classList.toggle("hide");
-    document.getElementById("skills").classList.toggle("show");
-}
